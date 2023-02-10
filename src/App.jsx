cb78023c245268122a5f56cb21efd5955d2bd588
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-02-10 07:16:16 +0700

    form validation using regex

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import logo from "./assets/logo.svg";
+import illustration from "./assets/illustration-dashboard.png";
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const emailValidation = () => {
+    const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
+    if (regEx.test(email)) {
+      setMessage(null);
+    } else if (!regEx.test(email) && email !== "") {
+      setMessage("Please provide a valid email address");
+    } else {
+      setMessage("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setEmail(e.target.value);
+  };
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="container mx-auto w-1/2">
+      <div className="mx-72 my-11">
+        <img src={logo} alt="logo" />
+      </div>
+      <div>
+        <h1 className="text-4xl text-center text-gray font-light">
+          We are launching{" "}
+          <span className="text-very-dark-blue font-bold">soon!</span>
+        </h1>
+        <p className="text-very-dark-blue text-center mt-4">
+          Subscribe and get notified
+        </p>
+      </div>
+      <div className="flex flex-row">
+        <div className="basis-2/4">
+          <input
+            className="rounded-3xl w-full mt-6 ml-24 w-80 border-pale-blue placeholder-pale-blue placeholder:text-sm"
+            type="email"
+            placeholder="Your email address"
+            value={email}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="basis-1/4">
+          <button
+            className="bg-blue text-white rounded-3xl w-36 h-10 ml-28 mt-6"
+            onClick={emailValidation}
+          >
+            Notify Me
+          </button>
+        </div>
+      </div>
+      <p>{message}</p>
+      <div>
+        <img src={illustration} alt="image" />
+      </div>
+      <div>
+        <div>
+          <FaFacebookF />
+        </div>
+        <div>
+          <FaTwitter />
+        </div>
+        <div>
+          <FaInstagram />
+        </div>
+      </div>
+      <footer>© Copyright Ping. All rights reserved.</footer>
+      <div>
+        <p class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="https://github.com/muhammadiqbalid83">Muhammad Iqbal</a>.
+        </p>
+      </div>
     </div>
   );
 }
